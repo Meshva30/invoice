d2d0608acc3b7d1b622240b1a25f3486c8bc0f67
meshva <5095meshwapatel@gmail.com>
2023-10-10 18:27:51 +0530

    9/10/2023

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,22 +1,21 @@
 //pd.ListView.builder(itemBuilder: (context, index) => pd.Container(
-  //        height: 80,
-    //      width: double.infinity,
-      //    padding: pd.EdgeInsets.all(10),
-        //  margin: pd.EdgeInsets.all(10),
-          //decoration: pd.BoxDecoration(
-            //border: pd.Border.all(color: PdfColors.black,width: 1.5,),
-          //),
-          //child: pd.Row(
-           // mainAxisAlignment: pd.MainAxisAlignment.spaceEvenly,
-            //children: [
-             // pd.Text('${productName[index]}',style:pd.TextStyle(fontSize: 20,), ),
-              //pd.Text('${productAmount[index]}',style:pd.TextStyle(fontSize: 20,), ),
-              //pd.Text('${productQty[index]}',style:pd.TextStyle(fontSize: 20,), ),
-            //],
-          //),
+//        height: 80,
+//      width: double.infinity,
+//    padding: pd.EdgeInsets.all(10),
+//  margin: pd.EdgeInsets.all(10),
+//decoration: pd.BoxDecoration(
+//border: pd.Border.all(color: PdfColors.black,width: 1.5,),
+//),
+//child: pd.Row(
+// mainAxisAlignment: pd.MainAxisAlignment.spaceEvenly,
+//children: [
+// pd.Text('${productName[index]}',style:pd.TextStyle(fontSize: 20,), ),
+//pd.Text('${productAmount[index]}',style:pd.TextStyle(fontSize: 20,), ),
+//pd.Text('${productQty[index]}',style:pd.TextStyle(fontSize: 20,), ),
+//],
+//),
 
 import 'package:flutter/material.dart';
-import 'package:invoice/pdfscreen.dart';
 import 'package:invoice/utils/productname.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pd;
@@ -99,31 +98,241 @@ Widget ProductBox(int index) {
 final pdf = pd.Document();
 
 generatePdf() async {
-  pdf.addPage(
-    pd.Page(
+  final netImage = await networkImage('https://cdn2.vectorstock.com/i/1000x1000/31/01/complete-payment-rubber-stamp-vector-12443101.jpg');
+
+  pdf.addPage(pd.Page(
       pageFormat: PdfPageFormat.a4,
       build: (context) {
         var index;
-        return pd.Column(
-
-          children: [
-            pd.Text("Bridal Studio",style: pd.TextStyle(fontSize: 30)),
-            pd.SizedBox(height: 20),
-            pd.Text("Marriage Shopping",style: pd.TextStyle(fontSize: 20)),
-            pd.SizedBox(height: 15),
-            pd.Text("NAME: Meshva Patel",style: pd.TextStyle(fontSize: 15)),
+        return pd.Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: pd.Column(children: [
+            pd.Container(
+              width: double.infinity,
+              height: 17,
+              alignment: pd.Alignment.center,
+              child: pd.Text(
+                'Bridal Studio',
+                style: pd.TextStyle(
+                    color: PdfColors.black,
+                    fontSize: 40,
+                    fontWeight: pd.FontWeight.bold),
+              ),
+            ),
+            pd.SizedBox(height: 30),
+            pd.Container(
+              alignment: pd.Alignment.centerLeft,
+              child: pd.Text(
+                'Marriage Shopping',
+                style: pd.TextStyle(
+                  color: PdfColors.black,
+                  fontSize: 30,
+                ),
+              ),
+            ),
             pd.SizedBox(height: 10),
-            pd.Text("Mobile: 9313220217",style: pd.TextStyle(fontSize: 15)),
-            pd.SizedBox(height: 10),
+            pd.Container(
+              alignment: pd.Alignment.centerLeft,
+              child: pd.Text(
+                'NAME : Meshva Patel',
+                style: pd.TextStyle(
+                  color: PdfColors.black,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            pd.Container(
+              alignment: pd.Alignment.centerLeft,
+              child: pd.Text(
+                'MOBILE : 9313220217',
+                style: pd.TextStyle(
+                  color: PdfColors.black,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            pd.SizedBox(height: 50),
+            pd.Container(
+              width: double.infinity,
+              child: pd.Row(children: [
+                pd.Container(
+                  alignment: pd.Alignment.center,
+                  height: 50,
+                  width: 130,
+                  color: PdfColors.grey,
+                  child: pd.Text('Product',
+                      style: pd.TextStyle(
+                          color: PdfColors.black,
+                          fontSize: 15,
+                          fontWeight: pd.FontWeight.bold)),
+                ),
+                pd.SizedBox(width: 5),
+                pd.Container(
+                  alignment: pd.Alignment.center,
+                  height: 50,
+                  width: 130,
+                  color: PdfColors.grey,
+                  child: pd.Text('Price',
+                      style: pd.TextStyle(
+                          color: PdfColors.black,
+                          fontSize: 15,
+                          fontWeight: pd.FontWeight.bold)),
+                ),
+                pd.SizedBox(width: 5),
+                pd.Container(
+                  alignment: pd.Alignment.center,
+                  height: 50,
+                  width: 50,
+                  color: PdfColors.grey,
+                  child: pd.Text('QTY',
+                      style: pd.TextStyle(
+                          color: PdfColors.black,
+                          fontSize: 15,
+                          fontWeight: pd.FontWeight.bold)),
+                ),
+                pd.SizedBox(width: 5),
+                pd.Container(
+                  alignment: pd.Alignment.center,
+                  height: 50,
+                  width: 50,
+                  color: PdfColors.grey,
+                  child: pd.Text('TAX',
+                      style: pd.TextStyle(
+                          color: PdfColors.black,
+                          fontSize: 15,
+                          fontWeight: pd.FontWeight.bold)),
+                ),
+                pd.SizedBox(width: 5),
+                pd.Container(
+                  alignment: pd.Alignment.center,
+                  height: 50,
+                  width: 130,
+                  color: PdfColors.grey,
+                  child: pd.Text('Amount',
+                      style: pd.TextStyle(
+                          color: PdfColors.black,
+                          fontSize: 15,
+                          fontWeight: pd.FontWeight.bold)),
+                ),
+              ]),
+            ),
+            pd.SizedBox(width: 20),
+            pd.Row(children: [
+              pd.Column(children: [
+                pd.Text('Blazer',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('Lehenga',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('Kurta',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('Chaniya choli',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('Tisat',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+              ]),
+              pd.SizedBox(width: 90),
+              pd.Column(children: [
+                pd.Text('1750',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('18000',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('800',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('5000',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('1800',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+              ]),
+              pd.SizedBox(width: 65),
+              pd.Column(children: [
+                pd.Text('1',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('1',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('12',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('2',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('2',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+              ]),
+              pd.SizedBox(width: 40),
+              pd.Column(children: [
+                pd.Text('18%',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('18%',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('18%',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('18%',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('18%',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+              ]),
+              pd.SizedBox(width: 45),
+              pd.Column(children: [
+                pd.Text('2065.0',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('21240.0',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('944.0',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('5900.0',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+                pd.Text('2124.0',
+                    style: pd.TextStyle(color: PdfColors.black, fontSize: 15)),
+                pd.SizedBox(height: 10),
+              ])
+            ]),
+            pd.Text(
+                '-------------------------------------------------------------------------------------------------------------------------'),
+            pd.SizedBox(width: 800),
+            pd.Container(
+              alignment: pd.Alignment.center,
+              height: 50,
+              width: 50,
+              decoration: pd.BoxDecoration(color: PdfColors.grey),
+              child: pd.Text('18',
+                  style: pd.TextStyle(color: PdfColors.black, fontSize: 20)),
+            ),
+            pd.SizedBox(height: 20,width: 50),
+            pd.Container(
+              alignment: pd.Alignment.center,
+              height: 50,
+              width: 150,
+              decoration: pd.BoxDecoration(color: PdfColors.grey),
+              child: pd.Text('Total Amount 27350.0/-',
+                  style: pd.TextStyle(color: PdfColors.black, fontSize: 20)),
+            ),
+               pd.Image(netImage),
+          ]),
 
-        ]
         );
-
-
-
-      }
-
-    )
-  );
+      }));
   await Printing.layoutPdf(onLayout: (format) async => await pdf.save());
 }
